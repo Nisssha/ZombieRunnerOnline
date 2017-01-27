b1df89931d1ce2ba9cf0c409573dcecf088b5948
Nisssha <nisha@vp.pl>
2017-01-27 11:51:28 +0100

    Zombie Runner

---
+++ Assets/Scripts/Eyes.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eyes : MonoBehaviour {
+
+	private Camera eyes;
+	// Update is called once per frame
+
+	void Start() {
+		eyes = gameObject.GetComponent<Camera>();
+	}
+
+	void Update () {
+		if (Input.GetButton("Zoom")) {
+			eyes.fieldOfView = 30f;
+		}else{
+			eyes.fieldOfView = 60f;
+		}
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+private Transform[] spawnPoints;
+private Transform spawnPoint;
+private GameObject spawnCollection;
+public bool reSpawn = false;
+public GameObject playerControler;
+
+	// Use this for initialization
+	void Start () {
+	spawnCollection = GameObject.Find("Player Spawn Points");
+	spawnPoints = spawnCollection.GetComponentsInChildren<Transform>();
+		
+	}
+
+	void Update () {
+		if (reSpawn){
+		spawnPoint = spawnPoints[Random.Range(1, spawnPoints.Length)];
+		Debug.Log(spawnPoint.position);
+		ReSpawn();
+		}
+	}
+
+void ReSpawn(){
+	Instantiate(playerControler, spawnPoint, false);
+	reSpawn = false;
+}
+
+}
